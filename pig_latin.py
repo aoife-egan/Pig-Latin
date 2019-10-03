4a3e8e10c10a505ca5c4287be09044d5e342ebc9
S00167887 <S00167887@mail.itsligo.ie>
2019-10-04 00:37:43 +0100

    word translation

--- pig_latin.py
+++ pig_latin.py
@@ -1,5 +1,13 @@
 original = input("Enter a word: ")
+
+pyg = "ay"
+word = original.lower()
+first = word[0]
+new_word = word + first + pyg
+new_word = new_word[1:len(new_word)]
+
 if len(original) > 0 and original.isalpha():
     print("You chose: %s to be translated." %(original))
+    print("Translation: %s" %(new_word))
 else:
     print("You didn't enter a word.")
